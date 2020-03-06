309fb1b190859f5ef1f555d7ce153e72a6687ec3
Ben Webb <ben@salilab.org>
2020-03-06 11:50:57 -0800

    Update URL for new PDB-Dev website

--- rnapolii/modeling/deposition.py
+++ rnapolii/modeling/deposition.py
@@ -334,6 +334,6 @@ try:
 except ImportError:
     import urllib2  # python2
 
-fh = urllib2.urlopen('https://pdb-dev.wwpdb.org/static/cif/PDBDEV_00000014.cif')
+fh = urllib2.urlopen('https://pdb-dev.wwpdb.org/cif/PDBDEV_00000014.cif')
 s, = ihm.reader.read(fh)
 print(s.title, s.restraints, s.ensembles, s.state_groups)
